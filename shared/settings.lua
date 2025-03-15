49136d170b735c04f50cfad48e8700160c57518a
levraimurphy <31287121+levraimurphy@users.noreply.github.com>
2025-03-15 14:05:46 +0100

    Update settings.lua

--- shared/settings.lua
+++ shared/settings.lua
@@ -1,9 +1,13 @@
 -- How to add a new style:
 -- 1. Copu img_samplke folder and modify .png files as you wish.
--- 2. Using GIMP, export then as .dds files.
+-- 2. Using GIMP or Paint.net, export then as .dds files.
+--     There are different formattings for dds files. People say for redm and gta its dxt5 but thats wrong its dxt10
+--     So you need to save and choose the format dxt10 when exporting .dds files
 -- 3. Using Codex explorer, make a new .ytd and put in all your dds files.
 -- 4. Put the .ytd in stream folder
 -- 5. put the name of your .ytd below under Style and enjoy !
+
+
 
 return {
     keybind = 0x8AAA0AD4, --- ALT
